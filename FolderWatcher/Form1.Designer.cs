90f7745f4437c82bc003d032a71b411ec2650120
stroev-ao <86775842+stroev-ao@users.noreply.github.com>
2021-07-02 08:30:52 +0700

    Add files via upload

---
+++ FolderWatcher/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace FolderWatcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.CMI_Notifications = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.MI_File = new System.Windows.Forms.ToolStripMenuItem();
+            this.MI_File_Export = new System.Windows.Forms.ToolStripMenuItem();
+            this.MI_View = new System.Windows.Forms.ToolStripMenuItem();
+            this.MI_View_History = new System.Windows.Forms.ToolStripMenuItem();
+            this.MI_View_Notifications = new System.Windows.Forms.ToolStripMenuItem();
+            this.MI_View_Minimize = new System.Windows.Forms.ToolStripMenuItem();
+            this.button4 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.contextMenuStrip.SuspendLayout();
+            this.menuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(219, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(237, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Browse...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(237, 54);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Watch";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(237, 54);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Stop";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.ContextMenuStrip = this.contextMenuStrip;
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "Folder Watcher [watching...]";
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_MouseClick);
+            // 
+            // contextMenuStrip
+            // 
+            this.contextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.CMI_Notifications});
+            this.contextMenuStrip.Name = "contextMenuStrip";
+            this.contextMenuStrip.Size = new System.Drawing.Size(175, 26);
+            this.contextMenuStrip.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip_Opening);
+            // 
+            // CMI_Notifications
+            // 
+            this.CMI_Notifications.Name = "CMI_Notifications";
+            this.CMI_Notifications.Size = new System.Drawing.Size(174, 22);
+            this.CMI_Notifications.Text = "Show Notifications";
+            this.CMI_Notifications.Click += new System.EventHandler(this.CMI_Notifications_Click);
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MI_File,
+            this.MI_View});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(324, 24);
+            this.menuStrip.TabIndex = 5;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // MI_File
+            // 
+            this.MI_File.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MI_File_Export});
+            this.MI_File.Name = "MI_File";
+            this.MI_File.Size = new System.Drawing.Size(37, 20);
+            this.MI_File.Text = "File";
+            this.MI_File.Click += new System.EventHandler(this.MI_File_Click);
+            // 
+            // MI_File_Export
+            // 
+            this.MI_File_Export.Name = "MI_File_Export";
+            this.MI_File_Export.Size = new System.Drawing.Size(158, 22);
+            this.MI_File_Export.Text = "Export History...";
+            this.MI_File_Export.Click += new System.EventHandler(this.MI_File_Export_Click);
+            // 
+            // MI_View
+            // 
+            this.MI_View.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MI_View_History,
+            this.MI_View_Notifications,
+            this.MI_View_Minimize});
+            this.MI_View.Name = "MI_View";
+            this.MI_View.Size = new System.Drawing.Size(44, 20);
+            this.MI_View.Text = "View";
+            // 
+            // MI_View_History
+            // 
+            this.MI_View_History.Name = "MI_View_History";
+            this.MI_View_History.Size = new System.Drawing.Size(180, 22);
+            this.MI_View_History.Text = "Show History";
+            this.MI_View_History.Click += new System.EventHandler(this.MI_View_History_Click);
+            // 
+            // MI_View_Notifications
+            // 
+            this.MI_View_Notifications.Name = "MI_View_Notifications";
+            this.MI_View_Notifications.Size = new System.Drawing.Size(180, 22);
+            this.MI_View_Notifications.Text = "Show Notifications";
+            this.MI_View_Notifications.Click += new System.EventHandler(this.MI_View_Notifications_Click);
+            // 
+            // MI_View_Minimize
+            // 
+            this.MI_View_Minimize.Name = "MI_View_Minimize";
+            this.MI_View_Minimize.Size = new System.Drawing.Size(180, 22);
+            this.MI_View_Minimize.Text = "Minimize";
+            this.MI_View_Minimize.Visible = false;
+            this.MI_View_Minimize.Click += new System.EventHandler(this.MI_View_Minimize_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 54);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Open Folder";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 83);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(300, 266);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.TabStop = false;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(324, 361);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(340, 400);
+            this.MinimumSize = new System.Drawing.Size(340, 128);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Folder Watcher";
+            this.Deactivate += new System.EventHandler(this.Form1_Deactivate);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip.ResumeLayout(false);
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem MI_View;
+        private System.Windows.Forms.ToolStripMenuItem MI_View_Notifications;
+        private System.Windows.Forms.ToolStripMenuItem MI_View_History;
+        private System.Windows.Forms.ToolStripMenuItem MI_View_Minimize;
+        private System.Windows.Forms.ToolStripMenuItem MI_File;
+        private System.Windows.Forms.ToolStripMenuItem MI_File_Export;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem CMI_Notifications;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ FolderWatcher/Form1.cs
@@ -0,0 +1,489 @@
+﻿using System;
+using System.IO;
+using System.Security.Permissions;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Threading;
+
+namespace FolderWatcher
+{
+    public partial class Form1 : Form
+    {
+        FileSystemWatcher watcher;
+
+        event HistoryChangedHandler HistoryChanged;
+        delegate void HistoryChangedHandler(object sender, EventArgs e);
+        event IconChangedHandler IconChanged;
+        delegate void IconChangedHandler(object sender, EventArgs e);
+
+        BackgroundWorker bw;
+
+        bool
+            folderSelected = false,
+            watching = false,
+            showHistory = true,
+            showNotifications = true,
+            saved = true,
+            minimized = false,
+            iconRed = false,
+            unreadNotifications = false;
+
+        bool UnreadNotifications
+        {
+            get { return unreadNotifications; }
+            set
+            {
+                unreadNotifications = value;
+
+                if (IconChanged != null)
+                {
+                    EventArgs eventArgs = new EventArgs();
+                    IconChanged(this, eventArgs);
+                }
+            }
+        }
+        
+        //byte l = 0b_0001_1100;
+
+        List<string> history;
+        string History
+        {
+            get { return history[history.Count - 1]; }
+            set
+            {
+                history.Add(value);
+
+                if (HistoryChanged != null)
+                {
+                    EventArgs eventArgs = new EventArgs();
+                    HistoryChanged(this, eventArgs);
+                }
+            }
+        }
+        string path = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            history = new List<string>();
+        }
+
+        [PermissionSet(SecurityAction.Demand, Name = "FullTrust")]
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            watcher = new FileSystemWatcher();
+
+            watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite
+                   | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+
+            //watcher.Filter = "*.txt";
+
+            watcher.Changed += new FileSystemEventHandler(OnChanged);
+            watcher.Created += new FileSystemEventHandler(OnChanged);
+            watcher.Deleted += new FileSystemEventHandler(OnChanged);
+            watcher.Renamed += new RenamedEventHandler(OnRenamed);
+
+            HistoryChanged = UpdateHistoryList;
+            IconChanged = UpdateNotifyIconText;
+            UpdateGUI();
+        }
+
+        private void UpdateGUI()
+        {
+            MI_File_Export.Enabled = history.Count > 0;
+            MI_View_History.Checked = showHistory;
+            MI_View_Notifications.Checked = CMI_Notifications.Checked = showNotifications;
+            this.Text = watching ? "Folder Watcher [watching...]" : "Folder Watcher";
+            this.Height = showHistory ? this.MaximumSize.Height : this.MinimumSize.Height;
+            notifyIcon.Text = watching ? "Folder Watcher [watching...]" : "Folder Watcher";
+            button1.Enabled = !watching;
+            button2.Visible = !watching;
+            button2.Enabled = folderSelected;
+            button3.Visible = button3.Enabled = watching;
+            button4.Enabled = folderSelected;
+            textBox1.ReadOnly = watching;
+            richTextBox1.Enabled = richTextBox1.Visible = showHistory;
+        }
+
+        private void UpdateHistoryList(object sender, EventArgs e)
+        {
+            BeginInvoke
+                (
+                    new Action
+                    (
+                        () =>
+                            {
+                                richTextBox1.Text += History + "\n";
+                                richTextBox1.SelectionStart = richTextBox1.Text.Length;
+                                richTextBox1.ScrollToCaret();
+                                saved = false;
+                            }
+                    )
+                );
+        }
+
+        private void UpdateNotifyIconText(object sender, EventArgs e)
+        {
+            BeginInvoke
+                (
+                    new Action
+                    (
+                        () =>
+                        {
+                            notifyIcon.Text = UnreadNotifications ? "You have unread notifications!" : watching ? "Folder Watcher [watching...]" : "Folder Watcher";
+                        }
+                    )
+                );
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (path != "" && history.Count > 0 && !saved)
+            {
+                DialogResult dr = MessageBox.Show("Save previous watching history?", "Question", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
+
+                switch(dr)
+                {
+                    case DialogResult.Yes:
+                        {
+                            SaveHistory();
+
+                            history.Clear();
+                            richTextBox1.Text = "";
+                            saved = true;
+
+                            break;
+                        }
+                    case DialogResult.No:
+                        {
+                            history.Clear();
+                            richTextBox1.Text = "";
+                            saved = true;
+
+                            break;
+                        }
+                }
+            }
+
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.ShowDialog();
+            path = fbd.SelectedPath;
+            fbd.Dispose();
+
+            if (path == "")
+                return;
+
+            textBox1.Text = path;
+
+            folderSelected = true;
+
+            UpdateGUI();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (watcher == null)
+                return;
+
+            try
+            {
+                watcher.Path = textBox1.Text;
+                watcher.EnableRaisingEvents = true;
+                History = "Watching folder: " + path + " at " + DateTime.Now.ToString();
+
+                button3.Focus();
+                watching = true;
+                UpdateGUI();
+                Minimize();
+            }
+            catch (ArgumentException ae)
+            {
+                MessageBox.Show(ae.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox1.Text = null;
+                folderSelected = false;
+                UpdateGUI();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (watcher == null)
+                return;
+
+            watcher.EnableRaisingEvents = false;
+            watching = false;
+            UpdateGUI();
+            button2.Focus();
+            History = "Stopped at " + DateTime.Now.ToString();
+        }
+
+        private void OnChanged(object source, FileSystemEventArgs e)
+        {
+            ShowBaloon("  " + e.ChangeType + ": " + e.FullPath);
+        }
+
+        private void OnRenamed(object source, RenamedEventArgs e)
+        {
+            ShowBaloon("  Renamed: " + e.OldFullPath + " to " + e.FullPath);
+        }
+
+        private void ShowBaloon(string _text, int _delay = 3000)
+        {
+            if (history != null)
+            {
+                History = _text + " at " + DateTime.Now.ToString();
+                
+                if (minimized)
+                {
+                    if (!IsThreadWorking())
+                        StartThread();
+                }
+
+                UnreadNotifications = true;
+            }
+
+            if (showNotifications)
+                notifyIcon.ShowBalloonTip(_delay, "Folder Watcher", _text, ToolTipIcon.None);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (history.Count > 0 && !saved)
+            {
+                DialogResult dr = MessageBox.Show("Save watching history?", "Folder Watcher", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
+
+                switch (dr)
+                {
+                    case DialogResult.Yes:
+                        SaveHistory();
+                        break;
+                    case DialogResult.Cancel:
+                        e.Cancel = true;
+                        break;
+                }
+            }
+        }
+
+        private void MI_View_Notifications_Click(object sender, EventArgs e)
+        {
+            showNotifications = !showNotifications;
+
+            UpdateGUI();
+        }
+
+        private void MI_File_Export_Click(object sender, EventArgs e)
+        {
+            SaveHistory();
+        }
+
+        private void MI_View_History_Click(object sender, EventArgs e)
+        {
+            showHistory = !showHistory;
+
+            UpdateGUI();
+        }
+
+        private void MI_View_Minimize_Click(object sender, EventArgs e)
+        {
+            Minimize();
+        }
+
+        private void Minimize()
+        {
+            this.WindowState = FormWindowState.Minimized;
+            this.ShowInTaskbar = false;
+            minimized = true;
+            if (showNotifications)
+                notifyIcon.ShowBalloonTip(3000, "Folder Watcher", "I'm here and still working!", ToolTipIcon.None);
+        }
+
+        private void MI_File_Click(object sender, EventArgs e)
+        {
+            MI_File_Export.Enabled = history.Count > 0;
+        }
+
+        private void notifyIcon_MouseClick(object sender, MouseEventArgs e)
+        {
+            switch (e.Button)
+            {
+                case MouseButtons.Left:
+                    {
+                        if (IsThreadWorking())
+                            StopThread();
+                        notifyIcon.Icon = Properties.Resources.Folder_Watcher1;
+                        this.WindowState = FormWindowState.Normal;
+                        this.ShowInTaskbar = true;
+                        minimized = false;
+                        UnreadNotifications = false;
+
+                        UpdateGUI();
+
+                        break;
+                    }
+                case MouseButtons.Right:
+                    {
+
+                        break;
+                    }
+            }
+        }
+
+        private void contextMenuStrip_Opening(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            CMI_Notifications.Checked = showNotifications;
+        }
+
+        private void Form1_Deactivate(object sender, EventArgs e)
+        {
+            if (this.WindowState == System.Windows.Forms.FormWindowState.Minimized)
+            {
+                Minimize();
+            }
+        }
+
+        private void CMI_Notifications_Click(object sender, EventArgs e)
+        {
+            showNotifications = !showNotifications;
+
+            UpdateGUI();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            folderSelected = textBox1.Text.Length > 0;
+
+            UpdateGUI();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string path = textBox1.Text;
+            
+            if (Directory.Exists(path))
+            {
+                Cursor.Current = Cursors.AppStarting;
+                Process.Start(new ProcessStartInfo("explorer.exe", path));
+                Cursor.Current = Cursors.Default;
+            }
+
+            path = null;
+        }
+
+        void SaveHistory()
+        {
+            int begin = path.LastIndexOf('\\') + 1;
+            string fileName = path.Substring(begin, path.Length - begin);
+            fileName += " " + DateTime.Now.Year + "." + DateTime.Now.Month + "." + DateTime.Now.Day + " " + DateTime.Now.Hour + "-" + DateTime.Now.Minute + "-" + DateTime.Now.Second;
+
+            SaveFileDialog sfd = new SaveFileDialog()
+            {
+                Filter = "Normal text file (*.txt)|*.txt",
+                FilterIndex = 0,
+                Title = "Export history",
+                FileName = fileName
+            };
+            DialogResult dr = sfd.ShowDialog();
+            switch (dr)
+            {
+                case DialogResult.OK:
+                case DialogResult.Yes:
+                    {
+                        StreamWriter sw = null;
+
+                        try
+                        {
+                            sw = new StreamWriter(sfd.FileName);
+
+                            foreach (string s in history)
+                            {
+                                sw.WriteLine(s);
+                            }
+
+                            saved = true;
+                        }
+                        catch (Exception e)
+                        {
+                            MessageBox.Show(e.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                        finally
+                        {
+                            sw.Dispose();
+                        }
+
+                        break;
+                    }
+            }
+
+            sfd.Dispose();
+        }
+
+        private void StartThread()
+        {
+            //если поток не создан, то создаем его
+            if (bw == null)
+            {
+                bw = new BackgroundWorker
+                {
+                    //позволяет прерывать поток
+                    WorkerSupportsCancellation = true
+                };
+
+                bw.DoWork += (s, e) =>
+                {
+                    BackgroundWorker worker = s as BackgroundWorker;
+
+                    while (true)
+                    {
+                        if (!worker.CancellationPending)
+                        {
+                            notifyIcon.Icon = iconRed ? Properties.Resources.Folder_Watcher1 : Properties.Resources.Folder_Watcher2;
+                            iconRed = !iconRed;
+
+                            Thread.Sleep(1000);
+                        }
+                        else
+                        {
+                            e.Cancel = true;
+                            return;
+                        }
+                    }
+                };
+
+                bw.RunWorkerCompleted += (s, e) =>
+                {
+                    if (e.Cancelled == true)
+                    {
+                        notifyIcon.Icon = Properties.Resources.Folder_Watcher1;
+                        iconRed = false;
+                    }
+                    else if (e.Error != null)
+                    {
+                        MessageBox.Show(e.Error.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+
+                    bw.Dispose();
+                };
+            }
+
+            //запускаем поток
+            bw.RunWorkerAsync();
+        }
+
+        private bool IsThreadWorking()
+        {
+            return bw == null ? false : bw.IsBusy;
+        }
+
+        private void StopThread()
+        {
+            if (bw == null)
+                return;
+
+            if (bw.WorkerSupportsCancellation)
+                bw.CancelAsync();
+        }
+    }
+}
